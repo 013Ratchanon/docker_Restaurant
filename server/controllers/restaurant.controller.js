9b425e475b3c4a9988c7380336e10609b4cfc1c0
013Ratchanon <664259013@webmail.npru.ac.th>
2025-07-14 17:09:10 +0700

    db

---
+++ server/controllers/restaurant.controller.js
@@ -0,0 +1,40 @@
+import Restaurant from "../models/restaurant.modal.js";
+const restaurantController = {};
+//Create and save a new restaurant
+restaurantController.create = async (req, res) => {
+  const { name, type, imgUrl } = req.body;
+  //validate data
+  if (!name || !type || !imgUrl) {
+    res
+      .status(400)
+      .send({ message: "Name, type or ImageUrl can nit be empty na ja !!" });
+    return;
+  }
+
+  await Restaurant.findOne({
+    where: {
+      name: name,
+    },
+  }).then((restaurant) => {
+    if (restaurant) {
+      res.status(400).send({ message: "Restaurant already exists!" });
+      return;
+    }
+    const newRestaurant = {
+      name: name,
+      type: type,
+      imgUrl: imgUrl,
+    };
+    Restaurant.create(newRestaurant)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((error) => {
+        res.status(500).send({
+          message:
+            error.message || "something error while creating the rastaurant",
+        });
+      });
+  });
+};
+export default restaurantController;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+import restaurantRouter from "./routers/restaurant.router.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API ");
 });
+
+//use routers
+app.use("/api/v1/restaurant", restaurantRouter);
 
 app.listen(PORT, () => {
   console.log("Listening to http://localhost:" + PORT);
